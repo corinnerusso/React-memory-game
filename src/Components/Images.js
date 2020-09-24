d953898056167ced70acc87f5cb23ac9e82aa80b
Corinne Russo <corinne.russo@gmail.com>
2020-09-24 11:23:15 +0200

    finish components

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,51 +1,84 @@
-import bird from "./Images/bird.png";
-import cockatoo from "./Images/cockatoo.png";
-import hoopoe from "./Images/hoopoe.png";
-import humming from "./Images/humming.png";
-import kingfisher from "./Images/kingfisher.png";
-import macaw from "./Images/macaw.png";
-import cardinal from "./Images/cardinal.png";
-import puffin from "./Images/puffin.png";
+import React from "react";
+import { images } from "./Images/index";
 
-export let images = [
-  {
-    name: "bird",
-    pic: bird,
-    flipped: false,
-  },
-  {
-    name: "cockatoo",
-    pic: cockatoo,
-    flipped: false,
-  },
-  {
-    name: "cardinal",
-    pic: cardinal,
-    flipped: false,
-  },
-  {
-    name: "hoopoe",
-    pic: hoopoe,
-    flipped: false,
-  },
-  {
-    name: "humming",
-    pic: humming,
-    flipped: false,
-  },
-  {
-    name: "kingfisher",
-    pic: kingfisher,
-    flipped: false,
-  },
-  {
-    name: "macaw",
-    pic: macaw,
-    flipped: false,
-  },
-  {
-    name: "puffin",
-    pic: puffin,
-    flipped: false,
-  },
-];
+class Images extends React.Component {
+  birds = [];
+
+  handleClick = (event) => {
+    let bird = event.target;
+    if (bird.getAttribute("check") === "found") {
+      return;
+    }
+
+    if (bird !== this.birds[0]) {
+      this.switch(bird);
+      this.birds.push(bird);
+    } else {
+      this.switch(bird);
+      this.birds = [];
+    }
+
+    if (this.birds.length > 2) {
+      if (!this.checkName(this.birds[0], this.birds[1])) {
+        this.switch(this.birds[0]);
+        this.switch(this.birds[1]);
+        this.birds.shift();
+        this.birds.shift();
+      } else {
+        this.birds.shift();
+        this.birds.shift();
+      }
+    }
+    let allPictures = document.getElementsByClassName("image-blank");
+    if (allPictures.length < 1) {
+      this.props.endGame(true);
+      let reset = document.getElementsByClassName("image");
+      for (let i = 0; i < reset.length; i++) {
+        reset[i].classList.add("image-blank");
+        reset[i].setAttribute("check", "false");
+        this.birds = [];
+      }
+    }
+  };
+
+  checkName = (bird1, bird2) => {
+    if (bird1.getAttribute("name") === bird2.getAttribute("name")) {
+      bird1.setAttribute("check", "found");
+      bird2.setAttribute("check", "found");
+      return true;
+    }
+    return false;
+  };
+
+  switch = (target) => {
+    if (target.getAttribute("check") === "true") {
+      target.setAttribute("check", "false");
+      target.classList.add("image-blank");
+    } else {
+      target.setAttribute("check", "true");
+      target.classList.remove("image-blank");
+    }
+  };
+
+  render() {
+    return (
+      <div className="images">
+        {images
+          .sort(() => Math.random() - 0.5)
+          .map((element) => {
+            return (
+              <div
+                className="image image-blank"
+                name={element.name}
+                style={{ background: `url(${element.pic})` }}
+                check="flase"
+                onClick={this.handleClick}
+              ></div>
+            );
+          })}
+      </div>
+    );
+  }
+}
+
+export default Images;
